65d988b5c96b2431060cffb5b54a8558c610a16b
jetblack87 <mwa29@drexel.edu>
2014-11-08 11:48:42 -0500

    initial add of the agent code.

---
+++ agent/src/github.com/jetblack87/apity/agent/agent.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+    "flag"
+    "fmt"
+)
+
+const APP_VERSION = "0.1"
+
+// The flag package provides a default help printer via -h switch
+var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+
+func main() {
+    fmt.Println("Hello, world");
+
+    flag.Parse() // Scan the arguments list 
+
+    if *versionFlag {
+        fmt.Println("Version:", APP_VERSION)
+    }
+}
+
