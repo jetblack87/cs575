b8880712db4480ad6ff50228f9232b25ea834f38
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2014-11-12 21:07:23 -0500

    finished loader and dumper

---
+++ agent/src/github.com/jetblack87/apity/agent/data/structs.go
@@ -0,0 +1,36 @@
+package data
+
+type Domain struct {
+    Name string
+    Key string
+    Runtime RuntimeConfig
+    Config StaticConfig
+}
+
+type RuntimeConfig struct {
+	Agents[] Agent
+}
+
+type StaticConfig struct {
+	Agents[] Agent
+	Processes[] Process
+}
+
+type Agent struct {
+	Name string
+	Key string
+	AgentClass string
+	OS string
+	Processes[] Process
+}
+
+type Process struct {
+	Name string
+	Key string
+	Command string
+	Arguments string
+	ProcessClass string
+	AdminState string
+	OperState string
+	Pid int64
+}
